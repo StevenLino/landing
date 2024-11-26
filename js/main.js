f4321e50416be1d1f2fed793878fad546a9dbd68
StevenLino <stemlino@espol.edu.ec>
2024-11-26 02:29:07 -0500

    Cambios Guía 09

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const databaseURL = 'https://landing-50966-default-rtdb.firebaseio.com/coleccion.json';
 
-let sendData = ( ) => {  
+let sendData = () => {
 
     // Obtén los datos del formulario
     const formData = new FormData(form);
@@ -18,36 +18,101 @@ let sendData = ( ) => {
         },
         body: JSON.stringify(data) // Convierte los datos a JSON
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.statusText}`);
-        }
-        return response.json(); // Procesa la respuesta como JSON
-    })
-    .then(result => {
-        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
-        form.reset()
-    })
-    .catch(error => {
-        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+            return response.json(); // Procesa la respuesta como JSON
+        })
+        .then(result => {
+            alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+            form.reset()
+
+            // Recuperación de datos
+            getData()
+        })
+        .catch(error => {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        });
 }
+let getData = async () => {
+    try {
+        // Realiza la petición fetch a la URL de la base de datos
+        const response = await fetch(databaseURL, {
+            method: 'GET'
+        });
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        }
+
+        // Convierte la respuesta en formato JSON
+        const data = await response.json();
+
+        if (data != null) {
+
+            // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+            let countSuscribers = new Map()
+
+            if (Object.keys(data).length > 0) {
+                for (let key in data) {
+
+                    let { email, saved } = data[key]
+
+                    let date = saved.split(",")[0]
+
+                    let count = countSuscribers.get(date) || 0;
+                    countSuscribers.set(date, count + 1)
+                }
+            }
+            // END
+
+            // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+            if (countSuscribers.size > 0) {
+
+                subscribers.innerHTML = ''
+       
+                let index = 1;
+                for (let [date, count] of countSuscribers) {
+                    let rowTemplate = `
+                        <tr>
+                            <th>${index}</th>
+                            <td>${date}</td>
+                            <td>${count}</td>
+                        </tr>`
+                    subscribers.innerHTML += rowTemplate
+                    index++;
+                }
+            }
+            // END
+
+        }
+    } catch (error) {
+        // Muestra cualquier error que ocurra durante la petición
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }
+}
+
 let ready = () => {
     console.log('DOM está listo')
+
+    // Recuperación de datos
+    getData();
 }
 
 let loaded = () => {
     let myform = document.getElementById('form');
-    
-    myform.addEventListener('submit', (eventSubmit) => {
-        eventSubmit.preventDefault(); 
-        
-        const emailElement = document.querySelector('.form-control-lg');
-           const emailText = emailElement.value;
 
-           if (emailText.length === 0) {
-             emailElement.focus()
-             emailElement.animate(
+    myform.addEventListener('submit', (eventSubmit) => {
+        eventSubmit.preventDefault();
+
+        const emailElement = document.querySelector('.form-control-lg');
+        const emailText = emailElement.value;
+
+        if (emailText.length === 0) {
+            emailElement.focus()
+            emailElement.animate(
                 [
                     { transform: "translateX(0)" },
                     { transform: "translateX(50px)" },
@@ -60,8 +125,8 @@ let loaded = () => {
                 }
             )
             return;
-           }
-           sendData();
+        }
+        sendData();
     })
 }
 
